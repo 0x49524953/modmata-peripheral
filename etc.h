5a78668e4105be12154f6ad53e3a9576bade951d
Iris Astrid <97922141+0x49524953@users.noreply.github.com>
2023-06-07 14:06:54 -0500

    More cleaning

---
+++ etc.h
@@ -0,0 +1,43 @@
+#include "Arduino.h"
+#include <util/crc16.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifndef ETC_H
+#define ETC_H
+
+static const uint16_t uint16_FROM_uint8_ptr(const uint8_t * ptr) {
+    // Warning: don't use this unless you know what you're doing
+    // if ptr does not ref a section of memory where there are at
+    // least two bytes allocated for it, it *will* segfault
+    return *(uint16_t *)(ptr);
+}
+
+static inline const uint16_t bswap16(const uint16_t w) {
+    // GCC extended inline ARM assembly snippet for swapping the bytes of 
+    // a word without using additional registers :3
+    uint16_t copy = w;
+    
+    __asm__ (
+        "eor %A0, %B0" "\n\t"
+        "eor %B0, %A0" "\n\t"
+        "eor %A0, %B0" "\n\t"
+        : "=r" (copy)
+        : "0" (copy)
+    );
+
+    return copy;
+}
+
+static inline const uint16_t crc16(const uint8_t * data, const size_t len) {
+    uint16_t crc = 0xffff;
+    for (int i=0; i < len; i++) {
+        // it came free with your fucking xbox
+        // https://github.com/avrdudes/avr-libc/blob/55e8cac69935657bcd3e4d938750960c757844c3/include/util/crc16.h#L113
+        crc = _crc16_update(crc, data[i]);
+    }
+
+    return crc;
+}
+
+#endif
